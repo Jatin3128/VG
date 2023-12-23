f9bc760d795b65be54ff5bfb6af663850296af5d
Jatin <jatinsarna64@gmail.com>
2023-12-23 23:23:09 +0530

    Jatin

--- vg_custom/override/di_payment.py
+++ vg_custom/override/di_payment.py
@@ -3,10 +3,38 @@ import frappe
 
 def di_payment_request_form(doc,method=None):
     if doc.workflow_state == "Pending from Accounts":
-        qi_doc = frappe.get_doc({
-                'doctype': 'Quality Inspection',
-                'job_reference_number': self.reference_number,
-                'service_reference_number': self.name,
-                'inspection_details':inspection_item
+        account_action_doc = frappe.get_doc({
+                'doctype': 'ToDo',
+                'allocated_to':"avnimistry01@gmail.com",
+                'description': doc.message,
+                'reference_name': doc.name,
+                'reference_type': 'DI Payment Request Form',
             })
-        qi_doc.insert()
+        account_action_doc.insert()
+    if doc.workflow_state == "pending from HO":
+            ho_action_doc = frappe.get_doc({
+                'doctype': 'ToDo',
+                'allocated_to':"12345.samir@gmail.com",
+                'description': doc.message,
+                'reference_name': doc.name,
+                'reference_type': 'DI Payment Request Form',
+            })
+            ho_action_doc.insert()
+    if doc.workflow_state == "Payment Pending":
+            paymet_action_doc = frappe.get_doc({
+                'doctype': 'ToDo',
+                'allocated_to':"avnimistry01@gmail.com",
+                'description': doc.message,
+                'reference_name': doc.name,
+                'reference_type': 'DI Payment Request Form',
+            })
+            paymet_action_doc.insert()
+        # if doc.workflow_state == "Payment Done":
+        #     paymet_action_doc = frappe.get_doc({
+        #         'doctype': 'ToDo',
+        #         'allocated_to':"12345.samir@gmail.com",
+        #         'description': doc.message,
+        #         'reference_name': doc.name,
+        #         'reference_type': 'DI Payment Request Form',
+        #     })
+        # paymet_action_doc.insert()
